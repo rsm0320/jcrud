c5058292248f4778837b3eeb9b3ec5d6b9b52562
Rafaella Moreira <116198194+rsm0320@users.noreply.github.com>
2022-12-16 14:50:16 -0300

    Update Jcrud.java
    Remoção dos comentários

--- src/jcrud/Jcrud.java
+++ src/jcrud/Jcrud.java
@@ -1,45 +1,30 @@
 package jcrud;
 
-/*
- * Importa todas as classes do Conector MySQL.
- * Download: https://dev.mysql.com/downloads/connector/j/
- * Na listagem, selecione "Platform Independent", baixe a versão ".ZIP" e 
- * descompacte-a. Copie o arquivo "mysql-connector-XXXX.jar" para uma pasta 
- * "recources" dentro da pasta do projeto.
- * Clique direito em "Bibliotecas" ? Adicionar JAR/Pasta...
- * Localize o arquivo do conector.
- */
 import java.sql.*;
 
 public class Jcrud {
 
-    /*
-     * Método para conexão com o banco de dados.
-     * Em breve, teremos uma classe somente para esse método e outros 
-     * relacionados ao MySQL.
-     */
     public static Connection mysqlConnect(
-            String hostname, // URL do servidor MySQL
-            String database, // Nome do banco de dados
-            String username, // Usuário desse banco de dados
-            String password // Senha do usuário do banco de dados
+            String hostname, 
+            String database, 
+            String username, 
+            String password 
     ) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Conecta a classe de banco de dados
-            return DriverManager.getConnection( // Fazer a conexão com o banco de dados
-                    "jdbc:mysql://" + hostname + ":3306/" + database, // String de conexão com o banco de dados
-                    username, // Usuário desse banco de dados
-                    password // Senha do usuário do banco de dados
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            return DriverManager.getConnection( 
+                    "jdbc:mysql://" + hostname + ":3306/" + database, 
+                    username, 
+                    password 
             );
-        } catch (Exception error) { // Tratamento de erros.
+        } catch (Exception error) {
             System.out.println("Oooops! Erro no banco de dados! " + error);
             return null;
         }
     }
 
-    // Método que executa o aplicativo
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("OlÃ¡ Mundo!");
     }
 
 }
